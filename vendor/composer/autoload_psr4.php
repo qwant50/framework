e0ae07be91f4cf09d9f8b43eb057ad4f227e190f
qwant50 <qwantonline@gmail.com>
2016-01-29 17:41:33 +0200

    qwant50/mailermodule added

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'qwantmailer\\' => array($vendorDir . '/qwant50/mailermodule/src'),
     'qwant50\\' => array($baseDir . '/classes'),
 );
